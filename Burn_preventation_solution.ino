aeb4862c017586878029afce7cf694a7e034dc42
Hangeol-Chang <57909592+Hangeol-Chang@users.noreply.github.com>
2021-07-27 16:11:21 +0900

    aa

--- Burn_preventation_solution.ino
+++ Burn_preventation_solution.ino
@@ -4,32 +4,68 @@
 #include "MLX90640_I2C_Driver.h"
 #include <avr/pgmspace.h>
 
+//=======================================wifi
+#include <SPI.h> 
+#include <WiFiNINA.h>
+//=======================================wifi
+
 #define EMMISIVITY 0.95
 #define TA_SHIFT 8 
 
 paramsMLX90640 mlx90640;
 const byte MLX90640_address1[] PROGMEM = { 0x33 }; //Default 7-bit unshifted address of the MLX90640
-const byte MLX90640_address2[] PROGMEM = { 0x32 }; //Default 7-bit unshifted address of the MLX90640
 
-static float tempValues1[32 * 24];
-static float tempValues2[32 * 24];
+static float tempValues1[24 * 32];
 
-int saveA1 = 0;
-int saveb1 = 0;
+int num = 0;
 
-int saveA2 = 0;
-int saveb2 = 0;
+float epsilon1_1 = 0;
+float epsilon1_2 = 0;
 
-int initval=0;
-float tempValues1_old[768];
-float tempValues2_old[768]; 
 
+//=======================================wifi
+bool danger = false;
+bool ing = false;
+bool tmpdanger = false;
+const char* ssid = "AndroidHotspot1867";       // 공백없이 정확히 넣어야 해요.
+const char* password = "01051781867";   // 공백없이 정확히 넣어야 해요.
+
+String s;
+//=======================================wifi
+
+int status = WL_IDLE_STATUS; 
+WiFiServer server(80); 
+WiFiClient client;
+
+while (status != WL_CONNECTED) { 
+      status = WiFi.begin(ssid, password); 
+      delay(1000); 
+      
+      Serial.println("wifi connected");
+      server.begin(); 
+    }
+
+    IPAddress ip = WiFi.localIP();
+    while (!client) { 
+      client = server.available(); 
+    }
+    
 void setup() {
     Serial.begin(115200);
+    //=================================================wifi
+    
+  
+    // 클라이언트로부터 데이터 수신을 기다림
+    Serial.println("new client"); 
+    //while(!client.available()){ 
+    //  delay(1); 
+    //}
+    
+    //=================================================wifi
+       
     Wire.begin();
     Wire.setClock(400000);
     Wire.beginTransmission((uint8_t)MLX90640_address1[0]);
-    Wire.beginTransmission((uint8_t)MLX90640_address2[0]);
     if (Wire.endTransmission() != 0) {
         Serial.println("MLX90640 not detected at default I2C address. Starting scan the device addr...");
         //    Device_Scan();
@@ -43,186 +79,175 @@ void setup() {
     int status2;
     uint16_t eeMLX90640[832];
     status1 = MLX90640_DumpEE(MLX90640_address1[0], eeMLX90640);
-    status2 = MLX90640_DumpEE(MLX90640_address2[0], eeMLX90640);
-    if (status1 != 0 || status2 != 0) Serial.println("Failed to load system parameters");
+    if (status1 != 0) Serial.println("Failed to load system parameters");
     status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
-    if (status1 != 0 || status2 != 0) Serial.println("Parameter extraction failed");
+    if (status1 != 0) Serial.println("Parameter extraction failed");
     MLX90640_SetRefreshRate(MLX90640_address1[0], 0x05);
-    MLX90640_SetRefreshRate(MLX90640_address2[0], 0x05);
     Wire.setClock(800000);
 }
 
 int del;
 void loop(void) {
-    readTempValues();
-    del = changedelay();
-    delay(del);
+    danger = readTempValues();
+    num++;
+
+    //=========================================wifi
+    client.flush();
+    //if (danger && !ing) {
+    if(true){
+      ing = true;
+      client.print(
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"  // the connection will be closed after completion of the response
+            "Refresh: 20\r\n"        // refresh the page automatically every 20 sec
+            "\r\n");
+      Serial.println("시발왜안돼");
+      delay(2); 
+      
+    }
+    
+    //=========================================wifi
+
 }
 
-int changedelay() {
-    int deltmp = 2000;
+bool readTempValues() {
+    int save1[24][32];
+    int n = 0;
+    if(num != 0){
+      for(int i = 0; i < 24; i++){    //이전 루프 012배열을 save배열에 저장(메모리 때문에 일차원배열에 저장)
+        for(int j = 0; j < 32; j++){
+         save1[i][j] = tempValues1[n];
+         n++;
+      }
+    }
+    }
 
-    return deltmp;
-}
-
-
-void readTempValues() {
-    float midmat1[768];
-    float midmat2[768];
-    float Atob1=0; float Atob2=0; 
-    float btoA1=0; float btoA2=0;
+  
     for (byte x = 0; x < 2; x++)
     {
         uint16_t mlx90640Frame1[834];
-        uint16_t mlx90640Frame2[834];
         int status1 = MLX90640_GetFrameData(MLX90640_address1[0], mlx90640Frame1);
-        int status2 = MLX90640_GetFrameData(MLX90640_address2[0], mlx90640Frame2);
-        if (status1 < 0 || status2 < 0)
+        if (status1 < 0)
         {
             Serial.print("GetFrame Error: ");
-            Serial.println(status1 + status2);
+            Serial.println(status1);
         }
 
         float vdd1 = MLX90640_GetVdd(mlx90640Frame1, &mlx90640);
-        float vdd2 = MLX90640_GetVdd(mlx90640Frame2, &mlx90640);
         float Ta1 = MLX90640_GetTa(mlx90640Frame1, &mlx90640);
-        float Ta2 = MLX90640_GetTa(mlx90640Frame2, &mlx90640);
         float tr1 = Ta1 - TA_SHIFT;
-        float tr2 = Ta2 - TA_SHIFT;
 
         MLX90640_CalculateTo(mlx90640Frame1, &mlx90640, EMMISIVITY, tr1, tempValues1);
-        MLX90640_CalculateTo(mlx90640Frame2, &mlx90640, EMMISIVITY, tr2, tempValues2);
     }
 
 
     int temp = 35;
+    int Atob = 0;
+    int btoA = 0;
 
-    Serial.println("\r\n===========================WaveShare MLX90640 Thermal Camera1===============================");
+    int cal[24][32];
 
-    int countA1 = 0;
-    int countb1 = 0;
-
-
-
-    int maxval = tempValues1[0];
-
-    for (int i = 0; i < 768; i++) {
-
-        if (((i % 32) == 0) && (i != 0)) {
-            Serial.println(" ");
-        }
-
-        Serial.print((int)tempValues1[i]);
-        Serial.print(" ");
-
-        if (maxval < tempValues1[i]) {
-            maxval = tempValues1[i];
-        }
-
-        if (tempValues1[i] >= temp) {
-            tempValues1[i] = 1;
-            countA1++;
-        }
-        else { tempValues1[i] = 0; }
-    }
-
-    Serial.println("Max Value is : " && maxval);
-  
-    for(int i = 0 ; i < 768 ; i++){
-    if(tempValues1[i] == 1) {
-      if ( i % 32 >= 1  && tempValues1[i-1] == 0) { tempValues1[i-1] = 5; } 
-      if ( i % 32 == 0  && tempValues1[i+1] == 0) { tempValues1[i+1] = 5; } 
-      
-      if ( i % 32 <= 30  && tempValues1[i+1] == 0) { tempValues1[i+1] = 5;}
-      if ( i % 32 == 31  && tempValues1[i-1] == 0) { tempValues1[i+1] = 5;}
-
-      if( i >= 32 && i <= 735 ) {
-        if (tempValues1[i-32] == 0) { tempValues1[i-32] = 5; }
-        if (tempValues1[i+32] == 0) { tempValues1[i+32] = 5; }
-      }
-      if( i < 32) {if (tempValues1[i+32] == 0) { tempValues1[i+32] = 5; }}
-      if( i > 735) {if (tempValues1[i-32] == 0) { tempValues1[i-32] = 5; }}
-      }
-    }
-    
+    epsilon1_2 = epsilon1_1;
 
 
+    //Serial.println("\r\n===========================WaveShare MLX90640 Thermal Camera1===============================");
 
-    Serial.println("\r\n===========================WaveShare MLX90640 Thermal Camera2===============================");
-    
-    int countA2 = 0;
-    int countb2 = 0;
-
-    maxval = tempValues2[0];
+    //Serial.println("Temperature array");
 
     for (int i = 0; i < 768; i++) {
 
         if (((i % 32) == 0) && (i != 0)) {
-            Serial.println(" ");
+            //Serial.println(" ");
         }
 
-        Serial.print((int)tempValues2[i]);
-        Serial.print(" ");
+        //Serial.print((int)tempValues1[i]);     //온도배열 출력
+        //Serial.print(" ");
 
-        if (maxval < tempValues2[i]) {
-            maxval = tempValues2[i];
+        if (tempValues1[i] >= temp) {       //기준온도 이상이면 0으로, 이하면 1로 변환
+            tempValues1[i] = 0;
         }
+        else { tempValues1[i] = 1; }
+    }
+    //Serial.println(" ");
+    
+    int arr[26][34];
 
-        if (tempValues2[i] >= temp) {
-            tempValues2[i] = 1;
-            countA2++;
+    for (int i = 0; i < 26; i++) {       //0과 1 배열을 테두리 프레임 추가된 2차원 배열로 변환
+        for (int j = 0; j < 34; j++) {
+            if (i * j == 0 || i == 25 || j == 33) {
+                arr[i][j] = 1;
+            }
+            else {
+                arr[i][j] = tempValues1[32 * (i - 1) + (j - 1)];
+            }
         }
-        else { tempValues2[i] = 0; }
+    }
+    //Serial.println("0,1,2 Array");
+    
+    for (int i = 1; i < 25; i++) {       //0과 인접한 1을 2로 변환
+        for (int j = 1; j < 33; j++) {
+            if (arr[i][j] == 1 && arr[i - 1][j] * arr[i + 1][j] * arr[i][j - 1] * arr[i][j + 1] == 0) {
+                arr[i][j] = 2;
+            }
+            //Serial.print(arr[i][j]);
+            //Serial.print(" ");
+        }
+        //Serial.println(" ");
+    }
+    //Serial.println(" ");
+
+    if(num!=0) {        //배열 위치대로 계산(나중배열 - 이전배열)
+        //Serial.println("Array - Save");
+        
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 32; j++) {
+              cal[i][j] = arr[i+1][j+1] - save1[i][j];
+              if(cal[i][j]>=0){
+                //Serial.print(" ");
+                //Serial.print(cal[i][j]);
+                //Serial.print(" ");
+              }
+              else{
+                //Serial.print(cal[i][j]);
+                //Serial.print(" ");
+              }
+              
+                if (cal[i][j] == -2) {   //b->A와 A->b 개수 카운트
+                    btoA++;
+                }
+                else if (cal[i][j] == 2) {
+                    Atob++;
+                }
+            }
+            //Serial.println(" ");
+        }
+        
+        epsilon1_1 = btoA / (Atob + 1)*1.0;
+        Serial.print("Epsilon1 is : ");
+        Serial.println(epsilon1_1);
+        Serial.print("Epsilon2 is : ");
+        Serial.println(epsilon1_2);
+        Serial.println("");
+    }
+        if(num >= 2){
+          if (epsilon1_1 >= 2 && epsilon1_2 >= 2) {
+            Serial.println("Warning!");
+            tmpdanger = true;
+          }else if ( tmpdanger == true) tmpdanger = false;
+        }
+    
+
+    n = 0;
+    for (int i = 0; i < 24; i++) {     //현재 0과1 배열을 일차원 배열에 저장(메모리 때문에)
+        for (int j = 0; j < 32; j++) {
+            tempValues1[n] = arr[i + 1][j + 1];
+            n++;
+        }
     }
 
-    Serial.println("Max Value is : " && maxval);
-
-    for(int i = 0 ; i < 768 ; i++){
-    if(tempValues2[i] == 1) {
-      if ( i % 32 >= 1  && tempValues2[i-1] == 0) { tempValues2[i-1] = 5; } 
-      if ( i % 32 == 0  && tempValues2[i+1] == 0) { tempValues2[i+1] = 5; } 
-      
-      if ( i % 32 <= 30  && tempValues2[i+1] == 0) { tempValues2[i+1] = 5;}
-      if ( i % 32 == 31  && tempValues2[i-1] == 0) { tempValues2[i+1] = 5;}
-
-      if( i >= 32 && i <= 735 ) {
-        if (tempValues2[i-32] == 0) { tempValues2[i-32] = 5; }
-        if (tempValues2[i+32] == 0) { tempValues2[i+32] = 5; }
-      }
-      if( i < 32) {if (tempValues2[i+32] == 0) { tempValues2[i+32] = 5; }}
-      if( i > 735) {if (tempValues2[i-32] == 0) { tempValues2[i-32] = 5; }}
-      }
-    }
-    if (initval==0){
-      for (int i=0; i<768; i++){
-
-        tempValues1_old[i]=tempValues1[i];
-        tempValues2_old[i]=tempValues2[i];
-      }
-      initval++;
-    }
-    else{
-      for (int i = 0; i < 768; i++){
-        midmat1[i]=tempValues1[i]-tempValues1_old[i];
-        if (midmat1[i]==4) {Atob1++;}
-        if (midmat1[i]==-4) {btoA1++;}
-        midmat2[i]=tempValues2[i]-tempValues2_old[i];
-        if (midmat2[i]==4) {Atob2++;}
-        if (midmat2[i]==-4) {btoA2++;}
-      } 
-      
-      float epsilon1=(btoA1)/(Atob1+1);
-      float epsilon2=(btoA2)/(Atob2+1);
-      
-      if (epsilon1 >=2 || epsilon2 >=2) {Serial.print("alert!");}
-      
-      for (int i = 0; i < 768; i++){
-        tempValues1_old[i]=tempValues1[i];
-        tempValues2_old[i]=tempValues2[i];
-      }
-    }
+    return tmpdanger;
 }
-    
 
 
 void Device_Scan() {
